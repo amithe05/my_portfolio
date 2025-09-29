28b6628e7facbdd17635bb59c3d6562972581d5c
AMITH E <amithe98@gmail.com>
2025-09-29 20:21:30 +0530

    removed_unwanted_

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'firebase_options.dart';
-import 'package:flutter_driver/driver_extension.dart';
 
 Future<void> main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
